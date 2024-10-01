ec551de0d7a364b32e214fa3d0cdcd462adfbcde
Rony Miranda <ronyemb@gmail.com>
2024-10-01 00:31:48 -0500

    Actualizando db.js para conexion sin variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,20 +1,22 @@
-import Sequelize from 'sequelize'
-import dotenv from 'dotenv';
-dotenv.config();
+import Sequelize from "sequelize";
+// import dotenv from 'dotenv';
+// dotenv.config();
 
- 
-const db = new Sequelize(process.env.DATABASE_URL, {
-
-    define: {
-        timestamps: false
-    },
-    pool:{
-        max :5,
-        min:0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorAliases: false
+// const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize("agenciaviajes", "root", "root", {
+  host: "127.0.0.1",
+  port: "3306",
+  dialect: "mysql",
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  operatorAliases: false,
 });
- 
+
 export default db;
